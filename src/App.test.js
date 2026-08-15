2f96b15850424732a5a41721da87a61c76b550fa
EC2 Default User <ec2-user@ip-172-31-10-106.ap-south-1.compute.internal>
2026-08-15 18:16:05 +0000

    Fix React test by mocking API

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('axios', () => ({
+  get: jest.fn(() =>
+    Promise.resolve({
+      data: {}
+    })
+  )
+}));
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
+
   ReactDOM.render(<App />, div);
+
   ReactDOM.unmountComponentAtNode(div);
 });
+
